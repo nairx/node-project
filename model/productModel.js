339648566d477b8529ab4307bf36c94faa9c3690
nairx <nairxprv@gmail.com>
2026-09-01 17:52:32 +0530

    updated

--- model/productModel.js
+++ model/productModel.js
@@ -1,7 +1,7 @@
 let products = [
-    { id: 1, name: "Product 1", price: 100, category: "electronics", imageUrl:"https://mystore37.vercel.app/p1.png" },
-    { id: 2, name: "Product 2", price: 120, category: "electronics", imageUrl:"https://mystore37.vercel.app/p2.png"  },
-    { id: 3, name: "Product 3", price: 140, category: "clothes", imageUrl:"https://mystore37.vercel.app/p3.png"  }
+    { id: 1, name: "Product 1", price: 100, description:"This is the description of the product", category: "electronics", imageUrl:"https://mystore37.vercel.app/p1.png" },
+    { id: 2, name: "Product 2", description:"This is the description of the product", price: 120, category: "electronics", imageUrl:"https://mystore37.vercel.app/p2.png"  },
+    { id: 3, name: "Product 3",description:"This is the description of the product", price: 140, category: "clothes", imageUrl:"https://mystore37.vercel.app/p3.png"  }
 ]
 
 export default products
